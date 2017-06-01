30c3d5e0e56415c62e76a7c035047ed31f5ccfb5
Luis Herrera <donluisherrera@example.com>
2017-06-01 00:36:07 -0700

    changed code to add statsd timer, counter and gauges

---
+++ IfCounter/ConsoleApp1/Counters.cs
@@ -0,0 +1,135 @@
+﻿using StatsdClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Counter
+
+    {
+
+       //constructor 
+        public Counter(String DirectoryPath)
+        {
+            _DirectoryPath = DirectoryPath;
+        }
+        String _DirectoryPath;
+
+        public IEnumerable<String> getFiles()
+        {
+            try {
+                return Directory.EnumerateFiles(_DirectoryPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+        }
+
+        public int countIfs(String file)
+        {
+            try
+            {
+                IEnumerable<string> lines = File.ReadLines(file);
+                var ifCounter = 0;
+                foreach (var l in lines)
+                {
+                    var words = l.Split(' ');
+                    foreach (var word in words)
+                    {
+                        if (word.ToLower().Equals("if"))
+                        {
+                            ifCounter++;
+                        }
+                    }
+                }
+                return ifCounter;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+        }
+
+        public int countCases(String file)
+        {
+            try
+            {
+                IEnumerable<string> lines = File.ReadLines(file);
+                var CaseCounter = 0;
+                foreach (var l in lines)
+                {
+                    var words = l.Split(' ');
+                    foreach (var word in words)
+                    {
+                        if (word.ToLower().Equals("case"))
+                        {
+                            CaseCounter++;
+                        }
+                    }
+                }
+                return CaseCounter;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+
+        }
+
+        public int countComments(string file)
+        {
+            try
+            {
+                IEnumerable<string> lines = File.ReadLines(file);
+                var CommentCounter = 0;
+                foreach (var l in lines)
+                {
+                    if (l.Contains("*"))
+                    {
+                        CommentCounter++;
+                    }
+                }
+                return CommentCounter;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+
+        }
+
+        public int countLines(String file)
+        {
+            try
+            {
+                IEnumerable<string> lines = File.ReadLines(file);
+                var lineCounter = 0;
+                foreach (var l in lines)
+                {
+                    lineCounter++;
+
+                }
+                return lineCounter;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+        }
+ 
+   
+}
+}
---
+++ IfCounter/ConsoleApp1/Runner.cs
@@ -0,0 +1,58 @@
+﻿using StatsdClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace ConsoleApp1
+{
+    class Runner
+    {
+        static void Main(string[] args)
+        {
+
+            var MetricsConfig = new MetricsConfig
+            {
+                StatsdServerName = "35.185.254.120",
+                StatsdServerPort = 8125,
+                Prefix = "Assignment2"
+            };
+
+            Metrics.Configure(MetricsConfig);
+
+            Console.WriteLine("Enter directory path: ");
+            var DirPath = Console.ReadLine();
+
+            var myProg = new Counter(DirPath); //
+
+            var Ifs = 0;
+            var Cases = 0;
+            var Comments = 0;
+            var lines = 0;
+
+            using (Metrics.StartTimer("processingFiles", 1))
+            {
+                var files = 0;
+                foreach (var file in myProg.getFiles())
+                {
+                    Ifs += myProg.countIfs(file);
+                    //Thread.Sleep(1);
+
+                    Cases += myProg.countCases(file);
+                    Comments += myProg.countComments(file);
+                    lines += myProg.countLines(file);
+
+                    Metrics.Counter("numberOfLines");
+                    Metrics.GaugeAbsoluteValue("GaugeFiles", files++);
+                }
+            }
+
+
+            Console.WriteLine("contains {0} If's, {1} Case's and {2} Comments ", Ifs, Cases, Comments);
+            Console.ReadKey();
+        }
+    }
+}
